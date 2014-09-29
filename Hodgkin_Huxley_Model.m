c6ebc22e4abcce279408e0214c31ae52e8d4d82c
Aidan <aidan.dowdle@gatech.edu>
2014-09-29 12:18:00 -0400

    Refactored code, added injection current input, added additional comments

--- Hodgkin_Huxley_Model.m
+++ Hodgkin_Huxley_Model.m
@@ -2,41 +2,44 @@
 % 9/27/14
 % ECE4784 Modeling Project
 % Phase I: Hodgkin Huxley Model
+
+%==========================================================================
+% Function name: Hodgkin_Huxley_Model
 %
-% Function Hodgkin_Huxley_Model provides a computational model for the
-% Hodgkin Huxley neuron model.
+% Function description: 
+% This function simulates the Hodgkin Huxley model for a variety of  
+% injection currents.
+%==========================================================================
 function Hodgkin_Huxley_Model
+close all;
+
 dt = 0.01; % Time step (ms)
 % 1. Simulate a steady-state neuron with the resting membrane potential. 
 %    Input current is zero.
-I = zeros(100/dt, 1);
-[V_m, g_K, g_Na, t] = run_model(I, dt);
-plot(t, V_m);
-
-figure(2);
-plot(t, g_K, 'b', t, g_Na, 'r');
+I_inj = zeros(100/dt, 1);
+run_model(I_inj, dt);
 
 % 2. Stimulate the model neuron with a step pulse of 5 uA/cm^2 for 0.5 ms
-I = [5*ones(1,0.5/dt) zeros(1, 99.5/dt + 1)];
-[V_m, g_K, g_Na, t] = run_model(I, dt);
-figure(3);
-plot(t, V_m);
-
-figure(4);
-plot(t, g_K, 'b', t, g_Na, 'r');
+I_inj = [5*ones(1,0.5/dt + 1) zeros(1, 99.5/dt)];
+run_model(I_inj, dt);
 
 % 3. Stimulate the model neuron with a constant current of 5 uA/cm^2
-I = 5*ones(1,100/dt+1);
-[V_m, g_K, g_Na, t] = run_model(I, dt);
-figure(5);
-plot(t, V_m);
-
-figure(6);
-plot(t, g_K, 'b', t, g_Na, 'r');
+I_inj = 5*ones(1,100/dt+1);
+run_model(I_inj, dt);
 
 end
 
-function [V_m, g_K, g_Na, t] = run_model(I, dt)
+%==========================================================================
+% Function name: run_model
+% Inputs (2): -I_inj, the injection current array in uA/cm^2
+%             -dt, the time step in ms
+%
+% Function description: 
+% This function runs the Hodgkin Huxley model for a given injection current 
+% and time step over 100 ms. The membrane voltage, K+ conductance, and Na+
+% conductance are plotted afterwards.
+%==========================================================================
+function run_model(I_inj, dt)
 % Initialize time vector and time step
 t = 0:dt:100; %Time (ms)
 
@@ -93,7 +96,7 @@ for i = 1:(length(t)-1)
     I_Na = g_Na(i)*(V_m(i) - E_Na);
     I_K = g_K(i)*(V_m(i) - E_K);
     I_L = g_L*(V_m(i) - E_L);
-    I_ion = I(i) - I_K - I_Na - I_L;
+    I_ion = I_inj(i) - I_K - I_Na - I_L;
     
     % Calculate membrane voltage, m, n, and h using Euler's method
     V_m(i+1) = V_m(i) + I_ion/C_m*dt;
@@ -103,4 +106,19 @@ for i = 1:(length(t)-1)
 end
 
 V_m = V_m + V_rest;
+
+% Plot membrane potential
+figure;
+plot(t, V_m)
+axis([0, 100, -100, 40])
+title('Membrane Potential')
+xlabel('Time (ms)')
+ylabel('Voltage (mV)')
+
+% Plot conductances
+figure;
+plot(t, g_Na, 'r', t, g_K, 'b');
+title('gK and gNa')
+xlabel('Time (ms)')
+ylabel('Conductance (S/cm2)')
 end
